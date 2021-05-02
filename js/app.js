ba56180cb4dffdd036cea9608c190cec79c0a6ab
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2021-05-02 23:50:43 +0530

    it works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,95 @@
-// CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+// const item = `
+// <li class="item">
+// <i class="fa fa-circle-thin co" job="complete" id="2"></i>
+// <p class="text ">gaaand mamamammamara</p>
+// <i class="fa fa-trash-o de" job="delete" id="0"></i>
+// </li>
+// `;
+// list.insertAdjacentHTML('beforeend', item);
+function addToDo(todo, id, done, trash = 0) {
+    if (trash) { return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `
+                <li class="item">
+                    <i class="fa fa-circle-thin co"" job="complete" id="0"></i>
+                    <p class="text ">${todo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                  </li>
+    `;
+
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
+        const toDo = input.value;
+
+        if (toDo) {
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+
+            });
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            id++;
+        }
+        input.value = "";
+
+
+    }
+});
+
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+list.addEventListener("click", function (event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob == "complete") {
+        completeToDo(element);
+    }
+    else {
+        removeToDo(element)
+    }
+
+});

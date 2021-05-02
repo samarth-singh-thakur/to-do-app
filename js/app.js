5642b4303987e8e51bdb5aab4742cf5a254d1c9f
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2021-05-02 23:58:19 +0530

    added local storage

--- js/app.js
+++ js/app.js
@@ -9,6 +9,21 @@ const LINE_THROUGH = "lineThrough";
 
 let LIST, id;
 
+let data = localStorage.getItem("TODO");
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+} else {
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array) {
+    array.forEach(function (item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
 
 clear.addEventListener("click", function () {
     localStorage.clear();
